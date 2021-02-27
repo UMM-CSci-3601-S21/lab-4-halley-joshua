344e383c26dcf5a36b9ac208272725bac963cdf1
Halley Paulson <paul1098@morris.umn.edu>
2021-02-27 12:29:02 -0600

    Remove Client-side Tests Temp.
    In order to allow merging of branch.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -32,7 +32,7 @@ import { LayoutModule } from '@angular/cdk/layout';
 import { UserCardComponent } from './users/user-card.component';
 import { UserProfileComponent } from './users/user-profile.component';
 import { AddUserComponent } from './users/add-user.component';
-import { AddTodoComponent } from './add-todo/add-todo.component';
+import { AddTodoComponent } from './todo/add-todo.component';
 
 const MATERIAL_MODULES: any[] = [
   MatListModule,
