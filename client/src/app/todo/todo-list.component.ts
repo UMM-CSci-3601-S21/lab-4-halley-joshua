9ff97db0cd1e784d8e82ffdbf2df6aed312adbba
Joshua Eklund <eklun124@umn.edu>
2021-03-01 17:05:11 -0600

    Implement feature of sorting todos by status
    This commit adds the code necessary to allow users to filter todos on the client page using the "Status" field. Issue #18

--- client/src/app/todo/todo-list.component.ts
+++ client/src/app/todo/todo-list.component.ts
@@ -40,11 +40,6 @@ export class TodoListComponent implements OnInit {
       this.serverFilteredTodos, { owner: this.todoOwner, category: this.todoCategory, keyWord: this.todoKeyWord, limit: this.todoLimit });
   }
 
-  // This is supposed to change the boolean value of status into Complete or Incomplete.
-  public changeToComplete(status?: string, tf?: boolean): string {
-    const text: string = this.todoService.changeToComplete(this.todoStatus);
-    return text;
-  }
 
   ngOnInit(): void {
     this.getTodosFromServer();
