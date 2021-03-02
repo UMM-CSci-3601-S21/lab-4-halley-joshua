f570d782a255d6c2dafa21c9d0812a702e6352ac
Joshua Eklund <eklun124@umn.edu>
2021-03-02 13:10:26 -0600

    Create forms for adding todos
    This commit adds the code necessary to create the forms for adding todos. Issue# 11

--- client/src/app/todo/add-todo.component.ts
+++ client/src/app/todo/add-todo.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { Todo } from './todo';
+import { TodoService } from './todo.service';
 
 @Component({
   selector: 'app-add-todo',
@@ -7,9 +12,100 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddTodoComponent implements OnInit {
 
-  constructor() { }
+  addTodoForm: FormGroup;
 
-  ngOnInit(): void {
+  todo: Todo;
+
+    // not sure if this name is magical and making it be found or if I'm missing something,
+  // but this is where the red text that shows up (when there is invalid input) comes from
+  addTodoValidationMessages = {
+    owner: [
+      {type: 'required', message: 'Owner is required'},
+      {type: 'minlength', message: 'Owner must be at least 2 characters long'},
+      {type: 'maxlength', message: 'Owner cannot be more than 50 characters long'},
+    ],
+
+    status: [
+      {type: 'required', message: 'Status is required'},
+      {type: 'boolean', message: 'Status must be a boolean'},
+    ],
+
+    category: [
+      {type: 'required', message: 'Category is required'},
+      {type: 'minlength', message: 'Category must be at least 2 characters long'},
+      {type: 'maxlength', message: 'Category cannot be more than 50 characters long'},
+    ],
+
+    body: [
+      {type: 'required', message: 'Body is required'},
+      {type: 'minlength', message: 'Body must be at least 2 characters long'},
+    ]
+  };
+
+  constructor(private fb: FormBuilder, private todoService: TodoService, private snackBar: MatSnackBar, private router: Router) {
+  }
+
+  createForms() {
+
+    // add user form validations
+    this.addTodoForm = this.fb.group({
+      // We allow alphanumeric input and limit the length for name.
+      owner: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(2),
+        // In the real world you'd want to be very careful about having
+        // an upper limit like this because people can sometimes have
+        // very long names. This demonstrates that it's possible, though,
+        // to have maximum length limits.
+        Validators.maxLength(50),
+      ])),
+
+      // Since this is for a company, we need workers to be old enough to work, and probably not older than 200.
+     status: new FormControl('', Validators.compose([
+        Validators.required,
+        // In the HTML, we set type="number" on this field. That guarantees that the value of this field is numeric,
+        // but not that it's a whole number. (The user could still type -27.3232, for example.) So, we also need
+        // to include this pattern.
+        Validators.pattern('^(true|false)$')
+      ])),
+
+       // We allow alphanumeric input and limit the length for name.
+       category: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(2),
+        // In the real world you'd want to be very careful about having
+        // an upper limit like this because people can sometimes have
+        // very long names. This demonstrates that it's possible, though,
+        // to have maximum length limits.
+        Validators.maxLength(50),
+      ])),
+
+      // We allow alphanumeric input and limit the length for name.
+      body: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(2)
+      ])),
+
+    });
+
+  }
+
+  ngOnInit() {
+    this.createForms();
+  }
+
+
+  submitForm() {
+    this.todoService.addTodo(this.addTodoForm.value).subscribe(newID => {
+      this.snackBar.open('Added Todo ' + this.addTodoForm.value.name, null, {
+        duration: 2000,
+      });
+      this.router.navigate(['/todos/', newID]);
+    }, err => {
+      this.snackBar.open('Failed to add the todo', 'OK', {
+        duration: 5000,
+      });
+    });
   }
 
 }
